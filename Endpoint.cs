a67b7b4a0993e5ad16ed4b20c417958923b452aa
Bartlomiej Komendarczuk <bkomendarczuk@gmail.com>
2022-01-27 22:47:43 +0100

    Bank account download has been added.

---
+++ Endpoint.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ERPXTpl
+{
+    internal class Endpoint
+    {
+        public static string BANK_ACCOUNTS = "https://app.erpxt.pl/api2/public/bankaccounts/";
+        public static string PRODUCTS = "https://app.erpxt.pl/api2/public/products/";
+        public static string CUSTOMERS = "https://app.erpxt.pl/api2/public/v1.2/customers/";
+        public static string AUTHORIZATION = "https://app.erpxt.pl/api2/public/token";
+    }
+}
