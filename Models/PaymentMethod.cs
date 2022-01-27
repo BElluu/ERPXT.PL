c9e768db1ec04cb29c73ab032dc8e98261736029
Bartlomiej Komendarczuk <bkomendarczuk@gmail.com>
2022-01-27 23:53:51 +0100

    Payment methods download has been added.

---
+++ Models/PaymentMethod.cs
@@ -0,0 +1,12 @@
+﻿using ERPXTpl.Enums;
+
+namespace ERPXTpl.Models
+{
+    public class PaymentMethod
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Primary { get; set; }
+        public PaymentMethodTypeEnum Type { get; set; }
+    }
+}
---
+++ Validators/PaymentMethodValidator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ERPXTpl.Validators
+{
+    internal class PaymentMethodValidator
+    {
+        internal static void GetPaymentMethodById(int paymentMethodId)
+        {
+            if (paymentMethodId == 0)
+            {
+                throw new ArgumentException("Id cannot be zero");
+            }
+        }
+    }
+}
