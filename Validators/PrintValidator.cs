6f20e03f656ae89f20f5a424a5046f04848be3ef
Bartlomiej Komendarczuk <bkomendarczuk@gmail.com>
2022-01-28 20:58:31 +0100

    Methods for handling printouts have been added.

---
+++ Validators/PrintValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ERPXTpl.Validators
+{
+    internal class PrintValidator
+    {
+        internal static void GetPrintValidator(int invoiceId)
+        {
+            if (invoiceId == 0)
+            {
+                throw new ArgumentException("Id cannot be zero");
+            }
+        }
+
+        internal static void GetCustomPrintValidator(int invoiceId, int printTemplateId)
+        {
+            if (invoiceId == 0 || printTemplateId == 0)
+            {
+                throw new ArgumentException("Id cannot be zero");
+            }
+        }
+    }
+}
